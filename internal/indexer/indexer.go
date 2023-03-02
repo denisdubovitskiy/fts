8b313b96fe6ee6e6927753325f1de3dfc41cf165
Denis Dubovitskiy <denisdubovitskiy@yandex.ru>
2023-03-02 12:03:56 +0400

    Verbose

--- internal/indexer/indexer.go
+++ internal/indexer/indexer.go
@@ -57,7 +57,7 @@ func (i *Indexer) Run(ctx context.Context) error {
 
 		if len(files) >= i.batchSize {
 			for _, f := range files {
-				log.Printf("indexer: indexing file %s\n", f)
+				log.Printf("indexer: indexing file %s\n", f.Path)
 			}
 			if err := i.storage.InsertFiles(ctx, files); err != nil {
 				return fmt.Errorf("indexer: unable to insert files: %v", err)
@@ -73,7 +73,7 @@ func (i *Indexer) Run(ctx context.Context) error {
 
 	if len(files) > 0 {
 		for _, f := range files {
-			log.Printf("indexer: indexing file %s\n", f)
+			log.Printf("indexer: indexing file %s\n", f.Path)
 		}
 		if err := i.storage.InsertFiles(ctx, files); err != nil {
 			return err
